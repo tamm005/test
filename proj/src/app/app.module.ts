87bae3f5c163334721b511b33db7bc332a98d5a4
TamaraCristaldo <tamaravcristaldo@gmail.com>
2017-11-09 11:25:12 -0300

    4

--- proj/src/app/app.module.ts
+++ proj/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './listado/listado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
